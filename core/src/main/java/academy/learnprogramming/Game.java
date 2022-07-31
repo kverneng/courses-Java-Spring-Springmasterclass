8f4320835221280b779a0c686d3714799f6ce0c3
Kverneng <robertkverneng@hotmail.com>
2022-07-31 15:28:02 +0200

    Adding project to git

---
+++ core/src/main/java/academy/learnprogramming/Game.java
@@ -0,0 +1,25 @@
+package academy.learnprogramming;
+
+public interface Game {
+    int getNumber();
+
+    int getGuess();
+
+    void setGuess(int guess);
+
+    int getSamllest();
+
+    int getBiggest();
+
+    int getRemainingGuesses();
+
+    void reset();
+
+    void check();
+
+    boolean isValidNumberRange();
+
+    boolean isGameWon();
+
+    boolean isGameLost();
+}
